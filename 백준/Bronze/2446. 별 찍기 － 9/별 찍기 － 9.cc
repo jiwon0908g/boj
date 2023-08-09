897bba07346a9af2180bdf86ffba926ace7ae96f
jiwon0908g <141632359+jiwon0908g@users.noreply.github.com>
2023-08-10 01:46:08 +0900

    [Bronze III] Title: 별 찍기 - 9, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2446. 별 찍기 － 9/별 찍기 － 9.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n,i,j;
+    cin >>n;
+
+    for (i=0;i<n;i++) {
+        for (j=0;j<=i-1;j++) {
+            cout << " ";
+        }
+        for (j=0;j<2*(n-i)-1;j++) {
+            cout << "*";
+        }
+        cout << "\n";
+    }
+    for (i=0;i<n-1;i++) {
+        for (j=0;j<n-i-2;j++) {
+           cout << " ";
+        }
+        for (j=0;j<2*i+3;j++) {
+            cout << "*";
+        }
+        cout << "\n";
+    }
+
+}
